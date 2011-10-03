077dcb4450765ef2927258b3a964cd28e0dbda4c
Neil T. Dantam <ntd@gatech.edu>
2011-10-03 14:21:31 -0400

    It builds!

---
+++ include/achros.hpp
@@ -0,0 +1,90 @@
+/* -*- mode: C; c-basic-offset: 2 -*- */
+/* ex: set shiftwidth=2 tabstop=2 expandtab: */
+/*
+ * Copyright (c) 2011, Georgia Tech Research Corporation
+ * All rights reserved.
+ *
+ * Author(s): Neil T. Dantam <ntd@gatech.edu>
+ * Georgia Tech Humanoid Robotics Lab
+ * Under Direction of Prof. Mike Stilman <mstilman@cc.gatech.edu>
+ *
+ *
+ * This file is provided under the following "BSD-style" License:
+ *
+ *
+ *   Redistribution and use in source and binary forms, with or
+ *   without modification, are permitted provided that the following
+ *   conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ *   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ *   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *   AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *   POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+
+#ifndef ACHROS_HPP
+#define ACHROS_HPP
+
+/** \file achros.cpp
+ */
+
+#include <pthread.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <ach.h>
+#include <ros/types.h>
+#include <ros/transport/transport.h>
+
+namespace ros
+{
+
+class TransportAch;
+typedef boost::shared_ptr<TransportAch> TransportAchPtr;
+
+/**
+ * \brief AchROS transport
+ */
+class /*ROSCPP_DECL*/ TransportAch /*: public Transport*/
+{
+public:
+  TransportAch( const char *channel_name );
+  virtual ~TransportAch();
+
+  /** \brief Create the channel */
+  int create(size_t index_slots, size_t msg_bytes);
+  /** \brief Open the channel */
+  int open();
+
+  /** \brief Close the channel */
+  int close();
+
+protected:
+  ach_channel_t chan_;
+  const char *chan_name_;
+};
+
+
+}
+
+
+#endif //ACHROS_HPP
